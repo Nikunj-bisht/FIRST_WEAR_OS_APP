506f1efeae4df51c489b535c3b40f42bb45b7dea
Nikunj bisht <nikkubisht12@gmail.com>
2021-08-21 01:56:43 +0530

    notification feature

--- src/main/java/com/animesafar/myapplication/MainActivity.java
+++ src/main/java/com/animesafar/myapplication/MainActivity.java
@@ -1,12 +1,20 @@
 package com.animesafar.myapplication;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
 import com.animesafar.myapplication.databinding.ActivityMainBinding;
 
@@ -15,7 +23,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private TextView mTextView;
     private ActivityMainBinding binding;
 private Button button;
-
+NotificationCompat.BigTextStyle bigTextStyle;
+NotificationManagerCompat notificationManagerCompat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +33,12 @@ private Button button;
         setContentView(binding.getRoot());
 
         mTextView = binding.text;
-
+ notificationManagerCompat = NotificationManagerCompat.from(this);
         button = findViewById(R.id.button);
         button.setOnClickListener(this);
+        bigTextStyle = new NotificationCompat.BigTextStyle()
+                           .setBigContentTitle("Hello")
+                .setSummaryText("Nikunj");
 
     }
 
@@ -36,10 +48,37 @@ private Button button;
 
         if(view.getId() == R.id.button){
 
-            Intent intent = new Intent(this,MyWatchFace.class);
-            startActivity(intent);
+            showotification();
 
         }
 
+    }
+
+    private void showotification(){
+
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    "channelid","My",NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setDescription("This is a notification");
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder noti = new NotificationCompat.Builder(this,"channelid");
+
+        Notification notification = noti.setStyle(bigTextStyle)
+              .setContentTitle("Hello")
+              .setContentText("Nikunj")
+              .setSmallIcon(R.drawable.ic_cc_clear)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.ic_cc_checkmark))
+        .setDefaults(NotificationCompat.DEFAULT_ALL)
+             .setColor(ContextCompat.getColor(getApplicationContext(),R.color.black_86p))
+              .setCategory(Notification.CATEGORY_REMINDER)
+              .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+                .build();
+        notificationManager.notify(2000,noti.build());
     }
 }
